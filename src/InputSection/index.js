210940384e6c0defde0438c5a53b2d42085715b0
TejaSimhaReddy3 <tejasimha1955@gmail.com>
2021-12-10 05:09:34 +0000

    typeError Commit

---
+++ src/InputSection/index.js
@@ -0,0 +1,43 @@
+import './index.css'
+
+const InputSection = props => {
+  const {itemDetails, showPassword, deleteItems} = props
+  const {id, websiteInput, passwordInput, userNameInput} = itemDetails
+
+  const initial = websiteInput ? websiteInput[0].toUpperCase() : ''
+
+  const showHidePasswd = showPassword ? (
+    <p className="passwordInp">{passwordInput}</p>
+  ) : (
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/password-manager-stars-img.png"
+      alt="stars"
+    />
+  )
+
+  const deleteList = () => {
+    deleteItems(id)
+  }
+
+  return (
+    <li className="listContainer">
+      <div className="initialContainer">{initial}</div>
+      <div className="textContainer">
+        <p className="webName">{websiteInput}</p>
+        <p className="userName">{userNameInput}</p>
+        {showHidePasswd}
+      </div>
+      <div className="buttonContainer">
+        <button className="button" onClick={deleteList} type="button">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/password-manager-delete-img.png"
+            className="deleteImg"
+            alt="delete"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default InputSection
---
+++ src/PasswordManager/index.js
@@ -0,0 +1,223 @@
+import {Component} from 'react'
+import {v4 as uuid} from 'uuid'
+import InputSection from '../InputSection'
+
+import './index.css'
+
+class PasswordManager extends Component {
+  state = {
+    websiteInput: '',
+    userNameInput: '',
+    passwordInput: '',
+    itemsList: [],
+    searchInput: '',
+    showPasswordChecked: false,
+  }
+
+  onChangeWebSite = event => {
+    this.setState({websiteInput: event.target.value})
+  }
+
+  onChangeUserName = event => {
+    this.setState({userNameInput: event.target.value})
+  }
+
+  onChangePassword = event => {
+    this.setState({passwordInput: event.target.value})
+  }
+
+  onSubmitForm = event => {
+    event.preventDefault()
+    const {websiteInput, userNameInput, passwordInput} = this.setState
+
+    const newItem = {
+      id: uuid(),
+      websiteInput,
+      userNameInput,
+      passwordInput,
+    }
+
+    this.setState(prevState => ({
+      itemsList: [...prevState.itemsList, newItem],
+      websiteInput: '',
+      userNameInput: '',
+      passwordInput: '',
+    }))
+  }
+
+  onSearchList = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  onCheckBox = () => {
+    this.setState(prevState => ({
+      showPasswordChecked: !prevState.showPasswordChecked,
+    }))
+  }
+
+  nullUsers = () => (
+    <div className="nullImgContainer">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/no-passwords-img.png"
+        alt="no passwords"
+        className="noPasswordImg"
+      />
+      <p className="noPasswd">No Passwords</p>
+    </div>
+  )
+
+  onDeleteItem = id => {
+    const {itemsList} = this.state
+
+    const updatedList = itemsList.filter(eachList => eachList.id !== id)
+
+    this.setState({itemsList: updatedList})
+  }
+
+  updatedLists = () => {
+    const {itemsList, searchInput} = this.state
+
+    const filteredList = itemsList.filter(eachList => {
+      eachList.webInput.toLowerCase().includes(searchInput.toLowerCase())
+      return eachList
+    })
+
+    return filteredList
+  }
+
+  render() {
+    const {
+      websiteInput,
+      userNameInput,
+      passwordInput,
+      showPasswordChecked,
+    } = this.state
+
+    const updatedList = this.updatedLists()
+
+    const userCount = updatedList.length
+
+    return (
+      <div className="bgContainer">
+        <div className="contentContainers">
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/password-manager-logo-img.png"
+            className="logoPasswdImg"
+            alt="app logo"
+          />
+          <div className="inputFormContainer">
+            <form onSubmit={this.onSubmitForm} className="form">
+              <h1 className="formHead">Add New Password</h1>
+              <div className="inputImgs">
+                <div className="imgContainers">
+                  <img
+                    src="https://assets.ccbp.in/frontend/react-js/password-manager-website-img.png"
+                    className="inputLogos"
+                    alt="website"
+                  />
+                </div>
+                <input
+                  type="text"
+                  onChange={this.onChangeWebSite}
+                  className="inputBox"
+                  placeholder="Enter Website"
+                  value={websiteInput}
+                />
+              </div>
+              <div className="inputImgs">
+                <div className="imgContainers">
+                  <img
+                    src="https://assets.ccbp.in/frontend/react-js/password-manager-username-img.png "
+                    className="inputLogos"
+                    alt="username"
+                  />
+                </div>
+                <input
+                  type="text"
+                  onChange={this.onChangeUserName}
+                  className="inputBox"
+                  placeholder="Enter Username"
+                  value={userNameInput}
+                />
+              </div>
+              <div className="inputImgs">
+                <div className="imgContainers">
+                  <img
+                    src="https://assets.ccbp.in/frontend/react-js/password-manager-password-img.png "
+                    className="inputLogos"
+                    alt="password"
+                  />
+                </div>
+                <input
+                  type="password"
+                  onChange={this.onChangePassword}
+                  placeholder="Enter Password"
+                  className="inputBox"
+                  value={passwordInput}
+                />
+              </div>
+              <div className="buttonContainer">
+                <button className="button" type="submit">
+                  Add
+                </button>
+              </div>
+            </form>
+            <img
+              src="https://assets.ccbp.in/frontend/react-js/password-manager-lg-img.png"
+              className="formImage"
+              alt="password manager"
+            />
+          </div>
+          <div className="passwordHandling">
+            <div className="footerHeader">
+              <p className="passwdCount">
+                Your Passwords <span className="countSpan">{userCount}</span>
+              </p>
+              <div className="searchContainer">
+                <img
+                  src="https://assets.ccbp.in/frontend/react-js/password-manager-search-img.png"
+                  alt="search"
+                  className="searchImg"
+                />
+                <input
+                  type="text"
+                  className="searchBox"
+                  onChange={this.onSearchList}
+                  placeholder="Search"
+                />
+              </div>
+            </div>
+            <hr className="footerHr" />
+            <div className="checkBoxContainer">
+              <input
+                type="checkbox"
+                checked={showPasswordChecked}
+                id="checkBox"
+                onChange={this.onCheckBox}
+              />
+              <label htmlFor="checkBox" className="checkBox">
+                Show passwords
+              </label>
+            </div>
+            {userCount === 0 ? (
+              this.nullUsers()
+            ) : (
+              <ul className="listsContainer">
+                {updatedList.map(eachList => (
+                  <InputSection
+                    key={eachList.id}
+                    itemDetails={eachList}
+                    showPassword={showPasswordChecked}
+                    deleteItems={this.onDeleteItem}
+                  />
+                ))}
+              </ul>
+            )}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default PasswordManager
